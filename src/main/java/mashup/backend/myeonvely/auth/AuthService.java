29f1be5bda20cdc5935271a815edf3b46717f064
daromKim <vo0a195@gmail.com>
2020-07-31 02:19:54 +0900

    fix : constructor error로 AuthService 수정

--- src/main/java/mashup/backend/myeonvely/auth/AuthService.java
+++ src/main/java/mashup/backend/myeonvely/auth/AuthService.java
@@ -2,7 +2,7 @@ package mashup.backend.myeonvely.auth;
 
 import io.jsonwebtoken.Claims;
 import lombok.RequiredArgsConstructor;
-import mashup.backend.myeonvely.function.FindUserByIdFunction;
+import mashup.backend.myeonvely.exception.UserDoseNotExistException;
 import mashup.backend.myeonvely.user.domain.User;
 import mashup.backend.myeonvely.user.domain.UserRepository;
 import org.springframework.beans.factory.annotation.Value;
@@ -15,12 +15,12 @@ public class AuthService {
     @Value("${jwt.secret}")
     private String secretKey;
 
-    private final FindUserByIdFunction findUserByIdFunction;
+    private final UserRepository userRepository;
 
     public User getUserByAccessToken(String accessToken){
         Claims claims = JwtTokenProvider.getInstance().decodingToken(accessToken, secretKey);
         Long userId = JwtTokenProvider.getInstance().getUserIdByClaims(claims, "AccessToken");
 
-        return findUserByIdFunction.apply(userId);
+        return userRepository.findById(userId).orElseThrow(UserDoseNotExistException::new);
     }
 }
--- src/main/java/mashup/backend/myeonvely/exception/UserDoseNotExistException.java
+++ src/main/java/mashup/backend/myeonvely/exception/UserDoseNotExistException.java
@@ -6,13 +6,13 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Getter
 @ResponseStatus(code = HttpStatus.BAD_REQUEST)
-public class UserNotFoundException extends BaseException {
+public class UserDoseNotExistException extends BaseException {
 
-    public UserNotFoundException(){
+    public UserDoseNotExistException(){
         this("User does not exist.");
     }
 
-    public UserNotFoundException(String message){
+    public UserDoseNotExistException(String message){
         super(ErrorCode.builder()
                 .httpStatus(HttpStatus.BAD_REQUEST)
                 .message("[ UserNotFoundException]\n" + message)
