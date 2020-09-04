65c3ceba0baf2b0ca0ffd1f5f20516c08fe24182
YJeongKim <yjeong20s@naver.com>
2020-09-04 22:15:21 +0900

    feat : 카테고리 등록 API

--- src/main/java/mashup/backend/tich/admin/controller/AdminCategoryController.java
+++ src/main/java/mashup/backend/tich/admin/controller/AdminCategoryController.java
@@ -4,13 +4,15 @@ import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import mashup.backend.tich.admin.service.AdminCategoryService;
 import mashup.backend.tich.item.dto.CategoryResponseDto;
+import mashup.backend.tich.item.dto.CategorySaveRequestDto;
+import mashup.backend.tich.user.domain.Role;
+import mashup.backend.tich.user.domain.User;
+import mashup.backend.tich.user.domain.UserRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.persistence.NoResultException;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -19,6 +21,7 @@ import java.util.List;
 public class AdminCategoryController {
 
     private final AdminCategoryService adminCategoryService;
+    private final UserRepository userRepository; /* 삭제 예정(관리자 확인용) */
 
     @ApiOperation("카테고리 목록 조회")
     @GetMapping
@@ -34,5 +37,35 @@ public class AdminCategoryController {
         CategoryResponseDto categoryResponseDto = adminCategoryService.showCategory(categoryId);
 
         return ResponseEntity.status(HttpStatus.OK).body(categoryResponseDto);
+    }
+
+    @ApiOperation("카테고리 등록")
+    @PostMapping
+    public ResponseEntity<CategoryResponseDto> saveCategory(@RequestHeader String accessToken,
+                                                            @RequestBody CategorySaveRequestDto requestDto) {
+        // 임시 코드 : 추후 수정
+        User user = makeTempUser();
+        // ToDo : user check (accessToken)
+
+        CategoryResponseDto categoryResponseDto = adminCategoryService.saveCategory(requestDto);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(categoryResponseDto);
+    }
+
+    /* 임시 코드 : 삭제 예정 */
+    private User makeTempUser() {
+        User user;
+        try {
+            user = userRepository.findByEmail("admin")
+                    .orElseThrow(() -> new NoResultException());
+        } catch (NoResultException e) {
+            user = userRepository.save(User.builder()
+                    .name("관리자")
+                    .email("admin")
+                    .picture("temp")
+                    .role(Role.ADMIN)
+                    .build());
+        }
+        return user;
     }
 }
